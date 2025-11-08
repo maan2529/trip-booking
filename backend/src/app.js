f168985bac909129614cf0c39ba1f4103199d0b5
Hemant <hemantrd2529@gmail.com>
2025-11-09 00:47:59 +0530

    dist added

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,28 +7,32 @@ import cors from "cors";
 import cookieParser from 'cookie-parser';
 import path from 'path';
 import { fileURLToPath } from 'url';
-
+  
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
+app.set('trust proxy', 1);
+  app.use(express.static(path.join(__dirname, "../public")));
+
+const environment_variables = process.env.FRONTEND_URL ? process.env.FRONTEND_URL : "http://localhost:5175";
 
 app.use(cors({
-  origin: "http://localhost:5173",  
-  credentials: true, 
+  origin: environment_variables,
+  credentials: true,
 }));
 
 app.use('/api/v1/auth', authRoutes);
 app.use('/api/v1/trips', tripRoutes);
 app.use('/api/v1/bookings', bookingRoutes);
 
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = path.dirname(__filename);
-// const clientPath = path.join(__dirname, '../client/dist');
 
-// app.use(express.static(clientPath));
-// app.get('*', (_, res) => res.sendFile(path.join(clientPath, 'index.html')));
 
+  app.use((req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  })
 app.use(globalErrorHandler);
 
 export default app;
